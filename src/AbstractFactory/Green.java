b9f6e3afa87d756d04f7ba2a2067a33bff265d4c
coconut <786016120@qq.com>
2019-02-11 23:33:47 +0800

    AbstractFactory Pattern
    for interview

---
+++ src/AbstractFactory/Green.java
@@ -0,0 +1,8 @@
+package AbstractFactory;
+
+public class Green implements Color{
+    @Override
+    public void fill() {
+        System.out.println("this is Green");
+    }
+}
---
+++ src/AbstractFactory/Square.java
@@ -0,0 +1,8 @@
+package AbstractFactory;
+
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("this is a Square");
+    }
+}
